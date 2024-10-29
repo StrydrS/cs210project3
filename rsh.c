09ebc2c705d9d8100669632cabf703916cf9b89b
StrydrS <strydr8@gmail.com>
2024-10-29 08:00:10 -0600

    working argument vector and isAllowed fcn

--- rsh.c
+++ rsh.c
@@ -11,13 +11,44 @@ extern char **environ;
 
 char *allowed[N] = {"cp","touch","mkdir","ls","pwd","cat","grep","chmod","diff","cd","exit","help"};
 
-int isAllowed(const char *cmd) {
-  int len = N;
-  for(int i = 0; i < len; i++) { 
-    if(!strcmp(cmd, allowed[i])) {
-      return 1;
+
+char **parseArg(const char *cmd) {
+  int len = strlen(cmd);
+  int numArgs = 1;
+  char *cmdCopy = (char *)malloc(sizeof(char) * len);
+  strcpy(cmdCopy, cmd);
+
+  for(int i = 0; i < len; i++) {
+    if((cmd[i] == ' ') & (cmd[i+1] != '\0')) {
+      numArgs++;
     }
   }
+
+  //allocate mem based on number of arguments
+  char **argV = malloc(numArgs * sizeof(char *));
+  for(int q = 0; q < numArgs; q++) { argV[q] = (char *)malloc(sizeof(char) * len); }
+
+  char *token = strtok(cmdCopy, " ");
+  int tokenNum = 0;
+
+  while(token != NULL) {
+    strcpy(argV[tokenNum], token);
+    token = strtok(NULL, " ");
+    tokenNum++;
+  }
+
+  return argV;
+}
+
+int isAllowed(const char *cmd) {
+  int len = N;
+
+  for(int i = 0; i < len; i++) { 
+      if(!strcmp(cmd, allowed[i])) {
+        return 1;
+      }
+    }
+
 	return 0;
 }
 
@@ -37,8 +68,14 @@ int main() {
 	if (strcmp(line,"\n")==0) continue;
 
 	line[strlen(line)-1]='\0';
+  
+  char **argV = parseArg(line);
 
-  printf("isAllowed: %d\n", isAllowed(line));
+  if(isAllowed(argV[0]) == 0) { 
+    printf("NOT ALLOWED!\n");
+  } else if(isAllowed(argV[0]) == 1) {
+    //POSIX STUFF
+    }
 
 	// TODO
 	// Add code to spawn processes for the first 9 commands
